21c0348af0ccd00790cce52ce5d947e7bd1b79d1
Muahammad Farhan <engrfarhansarfraz@gmail.com>
2024-09-30 00:34:48 +0500

    new file pattern added

---
+++ pattern.cpp
@@ -0,0 +1,153 @@
+   
+   // pattern 
+   
+//  #include <iostream>
+//  
+//  using namespace std;
+//  
+//  int main ()  {
+//  	
+//  int n;
+//  
+//  cout<<" please enetr the number : "<<endl;
+//  
+//  cin>>n;
+//  
+//  int i = 0;
+//  
+//  while( i<=n){
+//  	int j = 1;
+//  	while(j<=n){
+//	  
+//  	cout<<" * ";
+//  	
+//  	j++;
+//  	
+//  	
+//  }
+//  	cout<<endl;
+//  	
+//  	i++;
+//  	
+//  	
+//  }
+//  
+//  
+//  }
+//
+// #include <iostream>
+//  
+//  using namespace std;
+//  
+//  int main ()  {
+//  	
+//  int n;
+//  
+//  cout<<" please enetr the number : "<<endl;
+//  
+//  cin>>n;
+//  
+//  int i = 0;
+//  
+//  while( i<=n){
+//  	int j = 1;
+//  	while(j<=n){
+//	  
+//  	cout<<"*";
+//  	
+//  	j++;
+//  	
+//  }
+//  	cout<<endl;
+//  	
+//  	i++;	
+//  }
+//  
+//  }
+  
+//  
+// #include <iostream>
+//  
+//  using namespace std;
+//  
+//  int main ()  {
+//  	
+//  int n;
+//  
+//  cout<<" please enetr the number : "<<endl;
+//  
+//  cin>>n;
+//  
+//  int i = 1;
+//  
+//  while( i<=n){
+//  	int j = 1;
+//  	while(j<=n){
+//	  
+//  	cout<<i;
+//  	
+//  	j++;
+//  	
+//  }
+//  	cout<<endl;
+//  	
+//  	i++;	
+//  }
+//  
+//  }
+//   
+
+ 
+ #include <iostream>
+  
+  using namespace std;
+  
+  int main ()  {
+  	
+  int n;
+  
+  cout<<" please enetr the number : "<<endl;
+  
+  cin>>n;
+  
+  int i = 1;
+  
+  while( i<=n){
+  	int j = 1;
+  	while(j<=n){
+	  
+  	cout<<j;
+  	
+  	j++;
+  	
+  }
+  	cout<<endl;
+  	
+  	i++;	
+  }
+  
+  }
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
