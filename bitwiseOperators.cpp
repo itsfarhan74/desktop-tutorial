10d8006290050d00c17a9ed2e5e0a9d156a0c9b9
Muahammad Farhan <engrfarhansarfraz@gmail.com>
2024-10-10 22:44:17 +0500

    Bitwise Operators

---
+++ bitwiseOperators.cpp
@@ -0,0 +1,96 @@
+ 
+ #include<iostream>
+  
+  using namespace std;
+  
+  int main ()  {
+  	
+  	
+  	
+  	
+//  	
+//  	 int n;
+//  	 
+//  	 cout<<" Please eneter the number "<<endl;
+//  	 
+//  	 cin>>n;
+//  	 
+//  	 int sum = 0;
+//  	 
+//  	 for (int i=1; i<=n; i++){
+//  	 sum=sum+1;
+//  	
+//	   }
+//	   cout<< sum <<endl;
+	 
+
+  }
+  	
+  	
+//  	int a= 1;
+//  	int b=a++;
+//  	int c=++a;
+//  	cout<<b;
+//  	cout<<c;
+//  int n=3;
+//  cout<<(25*(++n));
+  
+  
+//  int  a=1;
+//   int  b=2;
+// if(a-->0  || ++b>2){
+// 	cout<<" hello ";
+//}
+//else{
+//		cout<<" he ";
+//}
+//  cout<<a<<" "<<b<<endl;
+   
+//  int  a=1;
+//   int  b=2;
+// if(a-->0  && ++b>2){
+// 	cout<<" hello ";
+//}
+//else{
+//		cout<<" he ";
+//}
+//  cout<<a<<" "<<b<<endl;
+  
+//  
+//  int  a,b=1;
+//  a=10;
+//  if(++a)
+//  	cout<<b;
+//  	else
+//	  
+//  	cout<<++b;
+  
+  
+  // post inrements               i++;
+  
+  // post decrement               i--;
+  
+   // pre inrements               ++i;
+  
+  // pre decrement               --i;
+  
+  
+//  int a = 2;
+//  
+//  int b = 4;
+//  
+//  cout<<" a&&b "<<(a&b)<<endl;
+//  
+//  cout<<" a|b "<<(a|b)<<endl;
+//  
+//  cout<<" ~b "<<(~b)<<endl;
+//  
+//  cout<<" a^b "<<(a^b)<<endl;
+  
+//  cout<<( 17>> 1)<<endl;
+//  cout<<( 5 >> 1)<<endl; 
+  
+  
+  
+  
+
