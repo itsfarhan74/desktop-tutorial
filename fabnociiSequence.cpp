67415f897cb04944f226e121d28ee90aa2f391a1
Muahammad Farhan <engrfarhansarfraz@gmail.com>
2024-10-10 23:20:41 +0500

    fabnocci

---
+++ fabnociiSequence.cpp
@@ -0,0 +1,134 @@
+    
+//     The Fibonacci sequence is a series of numbers where each number
+// is the sum of the two preceding ones, usually starting with 0 and 1. So, the sequence goes:
+   //0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+//   
+//   #include <iostream>
+//   
+//   using namespace std;
+//   
+//   int main (){
+//   	
+//   	int n=10;
+//   	
+//   	int a=0;
+//   	
+//   	int b=1;
+//   	cout<< a <<" "<< b <<" ";
+//   	for (int i=1;i<=n;i++){
+//   		
+//   	int nextNumber = a+b;
+//	   
+//	   a=b;
+//	   b=nextNumber;
+//	
+//	cout<<nextNumber<<" ";
+//	
+//	   }
+//   }
+   
+//   
+//   // prime or not 
+//   
+//   
+//    #include <iostream>
+//   
+//   using namespace std;
+//   
+//   int main (){
+//   	
+//   	int n;
+//   	
+//   	cin>>n;
+//   	
+//  bool isPrime=1;
+//   	
+//   	for (int i=2;i<n;i++){
+//   		
+//   	if(n%i==0) {  /// not a prime
+//   	
+//   	cout<<" not a prime number "<<endl;
+//   	
+//   	break;
+//   }
+//   	else{
+//   		cout<<" prime number "<<endl;
+//   		
+//   		break;6
+//  }
+//   }
+//}
+   
+   
+   //  If you want to break you can use continue it is used when you want to skip itration in the loop 
+
+
+//Itration in the sense when Luke begins 10 times every single time when you run in the code each part called itration
+
+
+   // concept of contiue to replace break option 
+   
+   
+   
+   // prime or not 
+   
+//   
+//    #include <iostream>
+//   
+//   using namespace std;
+//   
+//   int main (){
+//   	
+//   	int n;
+//   	
+//   	cin>>n;
+//   	
+//   	for ( int i=0;i<=n;i++){
+//   		cout<<" hello "<<endl;
+//   		
+//     	cout<<" Guys "<<endl;
+//				
+//	continue;
+//	
+//	 		cout<<" reply"<<endl;
+//					
+//	   }
+//}
+//
+//    
+//    #include <iostream>
+//   
+//   using namespace std;
+//   
+//   int main (){
+//   	
+//  
+//   	for ( int i=0;i<=5;i++){
+//   		cout<<i<<"  "<<endl;
+//   		i++;
+//   						
+//	   }
+//}
+   
+    
+    #include <iostream>
+   
+   using namespace std;
+   
+   int main (){
+   	
+  
+   	for ( int i=0;i<=5;i--  ){
+   		cout<<i<<"  "<<endl;
+   		i++;
+   						
+	   }
+}
+
+   
+
+
+
+
+
+
