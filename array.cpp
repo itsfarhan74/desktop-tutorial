b2211e7d699bbff0da701f88d6a51ea495eaa348
Muahammad Farhan <engrfarhansarfraz@gmail.com>
2024-11-17 16:38:20 +0500

    updaing the array

---
+++ array.cpp
@@ -0,0 +1,197 @@
+   
+// #include<iostream>
+// #include <climits>
+// using namespace std;
+//
+//int getMin(int num[], int n) {
+//    int min = INT_MAX;
+//    for (int i = 0; i < n; i++) {
+//        if (num[i] < min) {  // Corrected comparison to find the minimum
+//            min = num[i];
+//        }
+//    }
+//    return min;
+//}
+//
+//int getMax(int num[], int n) {
+//    int max = INT_MIN;
+//    for (int i = 0; i < n; i++) {
+//        if (num[i] > max) {
+//            max = num[i];
+//        }
+//    }
+//    return max;
+//}
+//
+//int main() {
+//    int size;
+//    cout << "Enter the size of the array: ";
+//    cin >> size;
+//    
+//    int num[100]; // Array to store numbers
+//
+//    // Take input for the array
+//    cout << "Enter " << size << " numbers:" << endl;
+//    for (int i = 0; i < size; i++) {
+//        cin >> num[i];
+//    }
+//
+//    // Displaying the max and min values
+//    cout << "Maximum value is: " << getMax(num, size) << endl;
+//    cout << "Minimum value is: " << getMin(num, size) << endl;
+//
+//    return 0;
+  
+  // updating the array 
+  
+//  #include<iostream>
+//  using namespace std;
+// void update(int arr[],int n){
+// 	cout<<" inside the function :"<<endl;
+//// updating the ARRAY
+// arr[0]=12;
+//
+//// now printing this array 
+//  for (int i=0;i<3;i++){
+//  	cout<<arr[i]<<" ";
+//  }
+//  cout<<endl;
+//  cout<<" going back in main "<<endl;
+// }
+// 
+//  int main(){
+//  int arr[3]=  {1,2,3};
+//  update(arr,3);
+//  
+//  // printing now 
+//  	cout<<" printing in main function :"<<endl;
+//  for (int i=0;i<3;i++){
+//  cout<<arr[i]<<" ";
+//  	
+//  }
+//  	cout<<endl;
+//  }
+  
+#include <iostream>
+using namespace std;
+
+int sumofArr(int arr[], int n) {
+    int sum = 0;
+    
+    // Calculate the sum of all elements
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];  // Add the value at arr[i] to sum
+    }
+    
+    return sum;  // Return the sum
+}
+
+int main() {
+    int size;
+    
+    // Get the size of the array
+    cout << "Enter size of array: ";
+    cin >> size;
+
+    // Declare the array
+    int arr[100];
+
+    // Input the elements of the array
+    cout << "Enter " << size << " elements: ";
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+
+    // Output the sum of the array
+    cout << "Sum of all array elements is: " << sumofArr(arr, size) << endl;
+
+    return 0;
+}
+
+  
+  
+  
+  
+  
+  
+// #include<iostream> 
+// using namespace std; // printing thee array
+// void printArray ( int arr[] , int size ){
+// cout<<" printing the array : "<<endl;
+// 	for (int i=0;i<size;i++){
+// 		cout<<arr[i]<<" ";
+//	 }
+//	 cout<<"printing done :";
+// }
+// int main (){
+// 	
+// 	int third[5]={2,4};
+// 	//int n=2;
+// 	printArray(third,3);
+// 	int size=sizeof(third)/sizeof(int);
+// 	cout<<" size of third is : "<<size<<endl;
+// 	
+// }
+// #include<iostream>
+// using namespace std; // printing thee array
+// void printArray ( int arr[] , int size ){
+// cout<<" printing the array : "<<endl;
+// 	for (int i=0;i<size;i++){
+// 		cout<<arr[i]<<" ";
+//	 }
+//	 cout<<"printing done :";
+// }
+// int main (){
+// 	
+// 	int third[5]={2,4};
+// 	//int n=2;
+// 	printArray(third,3);
+// 	
+// }
+ 
+// 	int arr[30]={1,2,3};
+// 	int n=30;
+// cout<<" printing the array : "<<endl;
+// 	for (int i=0;i<=n;i++){
+// 		cout<<arr[i]<<" ";
+//	 }
+//int arr[10]={0};
+// 	int n=30;
+// cout<<" printing the array : "<<endl;
+// 	for (int i=0;i<=n;i++){
+// 		cout<<arr[i]<<" ";
+//	 }
+// 
+//int arr[10];
+// 	int n=30;
+// cout<<" printing the array : "<<endl;
+// 	for (int i=0;i<=n;i++){
+// 		cout<<arr[i]<<" ";
+//	 }
+// }
+ 
+// #include<iostream>
+// 
+// using namespace std;
+// 
+// int main (){
+// 
+// 	int n[3]={2,33,21};
+// 
+// 	cout<<" value at 2 index : "<<n[2]<<endl;	
+//    cout<<" value at 0 index : "<<n[0]<<endl;
+//    cout<<" value at 7 index : "<<n[7]<<endl;
+// 
+// }
+// 
+ 
+// #include<iostream>
+// using namespace std;
+// int main (){
+// 	int n[10];
+// 	cout<<" value at 0 index : "<<n[0]<<endl;
+// 	cout<<" value at 1 index : "<<n[1]<<endl;
+// 	cout<<" value at 9 index : "<<n[9]<<endl;	
+// //	cout<<" value at 110 index : "<<n[110]<<endl;
+// 
+// }
