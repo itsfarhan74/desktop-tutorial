aaf9c213ece38d72e1e10e25f14371afdc59ccff
Farhan Sarfaraz <engrfarhansarfraz@gmail.com>
2025-02-22 23:07:25 +0500

    Static vs Dynamic Memory

---
+++ referenceVariable.cpp
@@ -0,0 +1,86 @@
+
+//#include<iostream>
+//using namespace std;
+//int main(){
+//    int i = 10;
+//    int &j = i;  //  creation of the reference variable 
+//
+//    cout<<i<<endl;
+//    i++;
+//    cout<<i<<endl;
+//    j++;
+//    cout<<i<<endl;
+//    i++;
+//    cout<<j<<endl;
+//    cout<<i<<endl;
+//    
+//}
+
+// #include<iostream>
+// using namespace std;
+// void update2(int &n){
+// 	n++;
+// }
+// void update(int n){
+// 	n++;
+// }
+// int main(){
+//    int n=10;
+//    
+//    cout<<"before update :"<<n<<endl;
+//    update(n);
+//    cout<<"after update :"<<n<<endl;
+//    
+//    cout<<"before update :"<<n<<endl;
+//    update2(n);
+//    cout<<"after update :"<<n<<endl;
+//}
+ 
+// #include<iostream>
+// using namespace std;
+// int main(){
+// 	
+// 	char ch=  'a';
+// 	
+// 	cout<<sizeof(ch)<<endl;
+// 	char *p =&ch;
+// 	cout<<sizeof(p)<<endl;
+// }
+//  
+//  #include<iostream>
+// using namespace std;
+// 
+// int getSum(int *arr,int n){
+// 	int sum=0;
+// 	for(int i=0;i<n;i++){
+// 		sum+=arr[i];	
+//	 }
+//	 return sum;
+// }
+// int main(){
+// 	
+// 	int n;
+// 	cin>>n;
+// 	
+// 	int* arr=new int[n];
+// 	for(int i=0;i<n;i++){
+// 		cin>>arr[i];
+//	 }
+//	 
+//	 int ans =getSum(arr,n);
+//	 
+//	 cout<<"sum of array is :"<<ans<<endl;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
